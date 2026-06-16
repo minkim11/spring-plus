1e64705183c652833a5d91d2af97ce3023f872aa
minkim11 <alsgur4772@gmail.com>
2026-06-16 21:47:42 +0900

    level1-3: JPA의 이해

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -17,6 +17,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -48,10 +51,14 @@ public class TodoService {
         );
     }
 
-    public Page<TodoResponse> getTodos(int page, int size) {
+    public Page<TodoResponse> getTodos(int page, int size, String weather, LocalDate startDate, LocalDate endDate) {
         Pageable pageable = PageRequest.of(page - 1, size);
 
-        Page<Todo> todos = todoRepository.findAllByOrderByModifiedAtDesc(pageable);
+        Page<Todo> todos = todoRepository.findAllByWeatherIsAndModifiedAtBetween(
+                pageable,
+                weather,
+                (startDate != null) ? startDate.atStartOfDay() : null,
+                (endDate != null) ? endDate.atTime(LocalTime.MAX) : null);
 
         return todos.map(todo -> new TodoResponse(
                 todo.getId(),
